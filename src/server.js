c91480b5d8594a494ad9603e53682247f2b8edff
Franklin Chieze <chieze.franklin@gmail.com>
2019-05-03 17:40:59 +0100

    fix issue with subscription

--- src/server.js
+++ src/server.js
@@ -36,17 +36,3 @@ const server = new GraphQLServer({
 server.start({ port: process.env.PORT || 5000 }, (options) => console.log(`Server is running on port ${options.port}`));
 
 // TODO: setInterval(graphWorker.findGraphsToUpdate, 1000 * 60);
-
-// const app = new express();
-// app.use(cors());
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(bodyParser.json());
-
-// app.get('/', async (req, res) => {
-//   res.status(200).send(`Hello World!<br /><br />Welcome to Live Pulse from ${req.connection.remoteAddress}`);
-// });
-
-// let server = app.listen(process.env.PORT || 5000, () => {
-//   let port = server.address().port;
-//   console.log(`Server started on port ${port}`)
-// })
