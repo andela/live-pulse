f58948e0a237236b2273352091e6d1356dc6ce56
waleCloud <walexrvp@gmail.com>
2019-05-10 18:36:05 +0100

    authentication token appended to all requests sent to the server

--- src/index.js
+++ src/index.js
@@ -4,30 +4,42 @@ import { ApolloClient } from 'apollo-boost';
 import { InMemoryCache } from 'apollo-boost';
 import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from 'react-apollo';
-// import { gql } from 'apollo-boost';
+import { gql } from 'apollo-boost';
 import './index.css';
 import Routes from './Routes';
 import * as serviceWorker from './serviceWorker';
+import { setContext } from 'apollo-link-context';
+import { AUTH_TOKEN } from './constants';
+
+const authLink = setContext((_,{ headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+});
 
 const HTTPLink = createHttpLink({
   uri: 'https://live-pulse-server.herokuapp.com/'
 });
 
 const client = new ApolloClient({
-  link: HTTPLink,
+  link: authLink.concat(HTTPLink),
   cache: new InMemoryCache()
 });
 
 // test the apollo query client, remove afterwards
-// client.query({
-//   query: gql`{
-//     dashboard(id: $ID) {
-//       title
-//       icon
-//       createdAt
-//     }
-//   }`
-// }).then(result => console.log(result));
+client.query({
+  query: gql`{
+    dashboard(id: $ID) {
+      title
+      icon
+      createdAt
+    }
+  }`
+}).then(result => console.log(result));
 //  end testing
 
 ReactDOM.render(
