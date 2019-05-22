785b89c62524a71eee123bf0c25606cd65f2ccaa
waleCloud <walexrvp@gmail.com>
2019-05-22 11:39:26 +0100

    create editable chart option menu

--- src/charts/BasicChart.js
+++ src/charts/BasicChart.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
+import { Card, CardHeader, IconButton, Menu, MenuItem } from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const BasicChart = ({data}) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
   if(data === null ){
     return (
       <div>
@@ -43,11 +47,40 @@ const BasicChart = ({data}) => {
       data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
     }],
   }
-  console.log(data);
+
+  const showMenu = (e) => {
+    setAnchorEl(e.currentTarget);
+  }
+
+  const closeMenu = () => {
+    setAnchorEl(null);
+  }
+
+  const open = Boolean(anchorEl);
   return (
-    <div>
+    <Card>
+      <CardHeader 
+        action={
+          <IconButton onClick={showMenu} 
+            aria-label="More" 
+            aria-haspopup={true}
+            aria-owns={open ? 'long-menu' : undefined}
+            >
+            <MoreVertIcon />
+          </IconButton>
+        }
+      />
+      <Menu
+        open={open}
+        onClose={closeMenu}
+        anchorEl={anchorEl}
+        >
+        <MenuItem onClick={closeMenu}>Edit</MenuItem>
+        <MenuItem onClick={closeMenu}>Settings</MenuItem>
+        <MenuItem onClick={closeMenu}>Delete</MenuItem>
+      </Menu>
       <HighchartsReact highcharts={Highcharts} options={chartOptions} />
-    </div>
+    </Card>
   )
   }
 }
